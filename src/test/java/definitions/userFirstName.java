ceb6ebc5f8b5ccfd23333d65dd4985bcc24c0926
Nazarii Demchuk <Nazarii@Alionas-MacBook-Pro.local>
2020-02-10 22:57:09 -0800

    fix

--- src/test/java/definitions/userFirstName.java
+++ src/test/java/definitions/userFirstName.java
@@ -2,6 +2,7 @@ package definitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.By;
 
 import static support.TestContext.getDriver;
@@ -18,7 +19,37 @@ public class userFirstName {
     }
 
     @And("I type first name {string}")
-    public void iTypeFirstName(String name) {
-        getDriver().findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(name);
+    public void iTypeFirstName(String firstName) {
+        getDriver().findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys(firstName);
+    }
+
+    @And("I type last name {string}")
+    public void iTypeLastName(String lastName) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys(lastName);
+    }
+
+    @And("I type email {string}")
+    public void iTypeEmail(String email) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(email);
+    }
+
+    @And("I type group {string}")
+    public void iTypeGroup(String group) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Group Code']")).sendKeys(group);
+    }
+
+    @And("I type password {string}")
+    public void iTypePassword(String passWord) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(passWord);
+    }
+
+    @And("I type confirm password {string}")
+    public void iTypeConfirmPassword(String confPassword) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Confirm Password']")).sendKeys(confPassword);
+    }
+
+    @Then("I click on Register Me button")
+    public void iClickOnRegisterMeButton() {
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
     }
 }
--- src/test/resources/features/Object/UserFirstName.feature
+++ src/test/resources/features/Object/UserFirstName.feature
@@ -1,4 +1,10 @@
 Feature: User - First Name
   Scenario: User - First Name - Alphanumeric and special char
     Given I open registration page
-    And I type first name "Nazar"
+    And I type first name "Nazar12!"
+    And I type last name "Demchuk"
+    And I type email "fsdfs@fsdg"
+    And I type group "p100"
+    And I type password "12345"
+    And I type confirm password "12345"
+    Then I click on Register Me button
